8c19d0d56dd053556aae799658d2679b11e59fbb
tolkjen <tolkjen@o2.pl>
2015-04-22 19:36:18 +0200

    basic mean/mode imputation

--- impute/R/impute.R
+++ impute/R/impute.R
@@ -1,3 +1,38 @@
-impute <- function() {
-  1
+impute <- function(x, ...) UseMethod("impute")
+
+impute.default <- function(data, ...) {
+  non_na_mean <- function(x) { 
+    mean(x[!is.na(x)]) 
+  }
+  
+  non_na_dominant <- function(x) {
+    if (!is.factor(x)) x <- factor(x)
+    A <- tabulate(x)
+    as.character(levels(x)[which.max(A)])
+  }
+  
+  model_func <- function(x) {
+    x <- as.vector(x)
+    if (mode(x) == "numeric")
+      non_na_mean(x)
+    else
+      non_na_dominant(x)
+  }
+  
+  model <- lapply(data, model_func)
+  class(model) <- "impute.model"
+  
+  model
+}
+
+predict.impute.model <- function(object, newdata=NULL, ...) {
+  replace_na <- Vectorize(function(x, value) {
+    if (is.na(x)) value else x
+  })
+  
+  col_list <- c(sapply(names(newdata), function(colname) {
+    list(replace_na(as.vector(newdata[[colname]]), object[[colname]]))
+  }))
+  
+  data.frame(col_list, row.names=seq(nrow(newdata)))
 }
