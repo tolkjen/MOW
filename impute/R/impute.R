6d3fc284e8afbc022f533401969cb89f9191fd17
tolkjen <tolkjen@o2.pl>
2015-04-22 20:44:48 +0200

    Enhancements
    Details:
     - impute takes two imputation methods as parameters
     - the impute() call is stored in the model
     - print support was added for the model

--- impute/R/impute.R
+++ impute/R/impute.R
@@ -1,28 +1,60 @@
 impute <- function(x, ...) UseMethod("impute")
 
-impute.default <- function(data, ...) {
-  non_na_mean <- function(x) { 
+impute.default <- function(data, mcont="mean", mnom="mode", ...) {
+  mean_model <- function(x) { 
     mean(x[!is.na(x)]) 
   }
   
-  non_na_dominant <- function(x) {
+  median_model <- function(x) {
+    median(x[!is.na(x)])
+  }
+  
+  mode_model <- function(x) {
     if (!is.factor(x)) x <- factor(x)
     A <- tabulate(x)
     as.character(levels(x)[which.max(A)])
   }
   
-  model_func <- function(x) {
+  if (mcont == "mean")
+    continous_model = mean_model
+  else if (mcont == "median")
+    continous_model = median_model
+  else
+    stop("mcont parameter must be 'mean' or 'median'.")
+  
+  if (mnom != "mode")
+    stop("mnom parameter must be equal to 'mode'")
+  
+  values <- lapply(data, function(x) {
     x <- as.vector(x)
     if (mode(x) == "numeric")
-      non_na_mean(x)
+      continous_model(x)
     else
-      non_na_dominant(x)
-  }
+      mode_model(x)
+  })
   
-  model <- lapply(data, model_func)
+  model = list(details=values, 
+               call=match.call(),
+               mcont=mcont,
+               mnom=mnom)
   class(model) <- "impute.model"
   
   model
+}
+
+print.impute.model <- function(x, ...)
+{
+  cat("Call:\n")
+  print(x$call)
+  
+  cat("\nImputation method (continous):\n")
+  print(x$mcont)
+  
+  cat("\nImputation method (nominal):\n")
+  print(x$mnom)
+  
+  cat("\nImputed values:\n")
+  print(data.frame(x$details), row.names=FALSE)
 }
 
 predict.impute.model <- function(object, newdata=NULL, ...) {
